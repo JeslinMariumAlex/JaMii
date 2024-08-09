5b55cab264f0deebcf79283eec9f5bb02a219f54
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-08-09 11:26:16 +0530

    React routing and rendering different pages based on a url endpoint

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/join">Join a Room</Link>
+            </li>
+            <li>
+              <Link to="/create">Create a Room</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<p>This is Home Page</p>} />
+          <Route path="/join" element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
+
